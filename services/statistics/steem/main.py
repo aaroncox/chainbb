632d38845bc5f8862440aff0697ed508e63feef9
Aaron Cox <aaron.cox@greymass.com>
2017-05-02 19:20:33 -0400

    Statistics integration (runs every 24hrs)

---
+++ services/statistics/steem/main.py
@@ -0,0 +1,45 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from pprint import pprint
+from pymongo import MongoClient
+import time
+import inspect
+import sys
+import os
+
+mongo = MongoClient("mongodb://mongo")
+db = mongo.forums
+
+def l(msg):
+    caller = inspect.stack()[1][3]
+    print("[FORUM][STATISTICS][{}] {}".format(str(caller), str(msg)))
+
+def update_statistics():
+  l("Updating stats for all forums...")
+  forums = db.forums.find()
+  for forum in forums:
+    l(forum['_id'])
+    stats = {
+      'posts': get_post_count(forum['tags']),
+      'replies': get_reply_count(forum['tags'])
+    }
+    l(stats)
+    db.forums.update({'_id': forum['_id']}, {'$set': {'stats': stats}})
+
+def get_post_count(tags = []):
+  return db.posts.count({'category': {'$in': tags}})
+
+def get_reply_count(tags = []):
+  return db.replies.count({'category': {'$in': tags}})
+
+if __name__ == '__main__':
+    l("Starting service")
+    update_statistics()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(update_statistics, 'interval', hours=24, id='update_statistics')
+    scheduler.start()
+    # Loop
+    try:
+        while True:
+            time.sleep(2)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
