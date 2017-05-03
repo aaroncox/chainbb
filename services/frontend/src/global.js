ec7a0ccde311d1ab2b80597ac30499b50f7bc8ce
Aaron Cox <aaron.cox@greymass.com>
2017-05-02 21:28:44 -0400

    HTTPS for main API

--- services/frontend/src/global.js
+++ services/frontend/src/global.js
@@ -1,6 +1,6 @@
 module.exports = {
   // REST_API: 'http://localhost:5000',
-  REST_API: 'http://api.chainbb.com:5000',
+  REST_API: 'https://api.chainbb.com:5000',
   STM_Config: {
     img_proxy_prefix: "https://steemitimages.com/",
     ipfs_prefix: false,
