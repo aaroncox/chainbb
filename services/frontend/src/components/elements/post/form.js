0ac6e8410463c1e36a750be88f0d0053fe7919fb
Aaron Cox <aaron.cox@greymass.com>
2017-05-04 15:27:52 -0400

    Only set an ID for the callback if it has a parent

--- services/frontend/src/components/elements/post/form.js
+++ services/frontend/src/components/elements/post/form.js
@@ -125,11 +125,14 @@ export default class PostForm extends React.Component {
             hasError: false
           })
           setTimeout(function() {
+            let id = false
             t.setState({
               waitingforblock: false
             })
             // If we have a parent, reload all of the children
             if(parent) {
+              // This had a parent, so set the callback to include it as the ID
+              id = account.name + '/' + permlink
               let parent_author = (parent.root_author) ? parent.root_author : parent.author,
                   parent_category = (parent.category) ? parent.category : 'tag',
                   parent_permlink = (parent.root_permlink) ? parent.root_permlink : parent.permlink
@@ -142,7 +145,6 @@ export default class PostForm extends React.Component {
                 permlink: parent_permlink
               })
             }
-            let id = account.name + '/' + permlink
             t.props.onComplete(id)
           }, 5000)
         }, 5000)
