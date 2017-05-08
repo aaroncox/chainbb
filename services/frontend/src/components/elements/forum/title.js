0b297e737345f831c2a29160eb09c4e579337fb3
Aaron Cox <aaron.cox@greymass.com>
2017-05-08 02:42:02 -0400

    Added instructions for tagging to forum header

--- services/frontend/src/components/elements/forum/title.js
+++ services/frontend/src/components/elements/forum/title.js
@@ -1,22 +1,34 @@
 
 import React from 'react';
 
-import { Button, Header, Segment } from 'semantic-ui-react'
+import { Button, Header, List, Segment } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 export default class ForumTitle extends React.Component {
   render() {
-    let forum = this.props.forum
+    let forum = this.props.forum,
+        tags = false
+    if(forum.tags) {
+      tags = forum.tags.map((tag, i) => <List.Item key={i}>
+        <Link to={`/topic/${tag}`}>
+          #{tag}
+        </Link>
+      </List.Item>)
+    }
     return (
       <div>
         <Segment basic>
           <Header size='huge'>
-            <Button floated='right' color='green' onClick={this.props.showNewPost}>
-              <i className='pencil icon'></i>
-              New Post
-            </Button>
             {forum.name}
             <Header.Subheader>
               {forum.description}
+            </Header.Subheader>
+            <Header.Subheader>
+              <p><em>To write a post in this forum, use one of the following tags as the <strong>first tag</strong>.</em></p>
+              <List horizontal small>
+                <List.Item header>Tags included:</List.Item>
+                {tags}
+              </List>
             </Header.Subheader>
           </Header>
         </Segment>
