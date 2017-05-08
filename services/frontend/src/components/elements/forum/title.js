9e0e875682213e1a57c05c8d04dc52553cf467f0
Aaron Cox <aaron.cox@greymass.com>
2017-05-08 17:02:33 -0400

    Reworked titles to show tags or accounts, whichever the forum is based on

--- services/frontend/src/components/elements/forum/title.js
+++ services/frontend/src/components/elements/forum/title.js
@@ -7,13 +7,41 @@ import { Link } from 'react-router-dom'
 export default class ForumTitle extends React.Component {
   render() {
     let forum = this.props.forum,
-        tags = false
-    if(forum.tags) {
+        accounts = false,
+        accounts_header = false,
+        tags = false,
+        tags_header = false
+    if(forum.accounts && forum.accounts.length > 0) {
+      accounts = forum.accounts.map((account, i) => <List.Item key={i}>
+        <Link to={`https://steemit.com/@${account}`}>
+          @{account}
+        </Link>
+      </List.Item>)
+      accounts_header = (
+        <Segment basic>
+          <em>This forum is filtered to show only posts from the following accounts.</em>
+          <br/>
+          <List horizontal size='small'>
+            {accounts}
+          </List>
+        </Segment>
+      )
+    }
+    if(forum.tags && forum.tags.length > 0) {
       tags = forum.tags.map((tag, i) => <List.Item key={i}>
         <Link to={`/topic/${tag}`}>
           #{tag}
         </Link>
       </List.Item>)
+      tags_header = (
+        <Segment basic>
+          <em>To write a post in this forum, use one of the following tags as the <strong>first tag</strong>.</em>
+          <br/>
+          <List horizontal size='small'>
+            {tags}
+          </List>
+        </Segment>
+      )
     }
     return (
       <div>
@@ -24,10 +52,8 @@ export default class ForumTitle extends React.Component {
               {forum.description}
             </Header.Subheader>
           </Header>
-          <p><em>To write a post in this forum, use one of the following tags as the <strong>first tag</strong>.</em></p>
-          <List horizontal size='small'>
-            {tags}
-          </List>
+          {tags_header}
+          {accounts_header}
         </Segment>
       </div>
     )
