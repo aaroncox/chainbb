f8c45e238a49d0cc20cbd6865028d4f8dacaab1c
Aaron Cox <aaron.cox@greymass.com>
2017-05-08 02:41:46 -0400

    Matching forum display to forums display

--- services/frontend/src/components/elements/forum/post.js
+++ services/frontend/src/components/elements/forum/post.js
@@ -27,7 +27,7 @@ export default class ForumHeader extends React.Component {
     if(topic.last_reply) {
       last_reply = (
         <Grid.Column width={4}>
-          <img alt='{topic.last_reply_by}' src={`https://img.steemconnect.com/@${topic.last_reply_by}?size=35`} className="ui rounded floated left mini image" style={{minHeight: '35px', minWidth: '35px'}}/>
+          <img alt='{topic.last_reply_by}' src={`https://img.steemconnect.com/@${topic.last_reply_by}?size=35`} className="ui rounded floated left mini image" style={{minHeight: '35px', minWidth: '35px', marginBottom: 0}}/>
           <UserLink username={topic.last_reply_by} />
           <br/>
           <TimeAgo date={`${topic.last_reply}Z`} />
@@ -37,9 +37,9 @@ export default class ForumHeader extends React.Component {
     return (
       <Segment attached key={topic._id}>
         <Grid stackable>
-          <Grid.Row>
+          <Grid.Row verticalAlign='middle'>
             <Grid.Column width={10}>
-              <Header size='medium'>
+              <Header size='small'>
                 <Link to={`${topic.url}`}>
                   {topic.title}
                 </Link>
@@ -52,10 +52,12 @@ export default class ForumHeader extends React.Component {
                 </Header.Subheader>
               </Header>
             </Grid.Column>
-            {last_reply}
             <Grid.Column width={2} only='large screen' className="center aligned">
-              {topic.children}
+              <Header size='small'>
+                {topic.children}
+              </Header>
             </Grid.Column>
+            {last_reply}
           </Grid.Row>
         </Grid>
       </Segment>
