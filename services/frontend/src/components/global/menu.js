2d1154f2f13629404d03d445bee417f74c92df1f
Aaron Cox <aaron.cox@greymass.com>
2017-05-08 03:46:37 -0400

    Removed old height code

--- services/frontend/src/components/global/menu.js
+++ services/frontend/src/components/global/menu.js
@@ -4,45 +4,15 @@ import { Link } from 'react-router-dom'
 import * as GLOBAL from '../../global';
 
 export default class HeaderMenu extends Component {
-  state = {}
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  constructor(props) {
-    super(props)
-    this.state = { height: '' }
-    this.getHeight = this.getHeight.bind(this)
-  }
-
-  componentDidMount() {
-    this.getHeight()
-  }
-
-  async getHeight() {
-    try {
-      const response = await fetch(`${ GLOBAL.REST_API }/height`)
-      if (response.ok) {
-        const result = await response.json()
-        this.setState({
-          height: result.data.value
-        })
-      } else {
-        console.error(response.status)
-      }
-    } catch(e) {
-      console.error(e)
-    }
-  }
-
-
   render() {
-    const { height } = this.state
     return (
-      <Menu inverted>
+      <Menu color='blue' inverted>
         <Container>
-          <Link to='/' className='title item'>chainBB</Link>
-          <Link to='/' className='title active item'>Forums</Link>
-          <Menu.Item position='right'>Block Height: {height}</Menu.Item>
+          <Link to='/' className='title active item'>
+            <strong>chainBB.com</strong>
+          </Link>
+          <Link to='/' className='title item'>General</Link>
+          <Link to='/' className='title item'>Crypto</Link>
         </Container>
       </Menu>
     )
