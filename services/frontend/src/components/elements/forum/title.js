c61ccebd6d2de6d155b6837a628171cff2bfb62a
Aaron Cox <aaron.cox@greymass.com>
2017-06-15 01:23:58 -0400

    Making the header stand out more + showing who manages the forum (hardcoded)

--- services/frontend/src/components/elements/forum/title.js
+++ services/frontend/src/components/elements/forum/title.js
@@ -1,18 +1,33 @@
 
 import React from 'react';
 
-import { Header } from 'semantic-ui-react'
+import { Grid, Header, Segment } from 'semantic-ui-react'
+
+import AccountLink from '../account/link'
 
 export default class ForumTitle extends React.Component {
   render() {
     let forum = this.props.forum
     return (
-      <Header size='huge' key={forum._id}>
-        {forum.name}
-        <Header.Subheader>
-          {forum.description}
-        </Header.Subheader>
-      </Header>
+      <Segment color="blue">
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Header size='huge' key={forum._id}>
+                {forum.name}
+                <Header.Subheader>
+                  {forum.description}
+                </Header.Subheader>
+              </Header>
+            </Grid.Column>
+            <Grid.Column width={8} textAlign="right">
+              Managed by
+              <br/>
+              <AccountLink username='chainbb' />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
     )
   }
 }
