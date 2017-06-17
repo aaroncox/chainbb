60d21397583e4291983c943dbd2c33a898907403
Aaron Cox <aaron.cox@greymass.com>
2017-06-17 02:18:11 -0400

    Reworked the header to match the style

--- services/frontend/src/components/elements/forum/title.js
+++ services/frontend/src/components/elements/forum/title.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import { Grid, Header, Segment } from 'semantic-ui-react'
 
@@ -7,20 +8,44 @@ import AccountLink from '../account/link'
 
 export default class ForumTitle extends React.Component {
   render() {
-    let forum = this.props.forum
+    let { attached, forum } = this.props
+    let tags = false
+    let subheader = false
+    if(forum.tags) {
+      tags = (
+        <Header.Subheader style={{marginTop: '0.25em'}}>
+          {' ↳ '}
+          {forum.tags.map((tag, i) => <span key={i}>
+            {' '}
+            <Link to={`/topic/${tag}`}>
+              #{tag}
+            </Link>
+          </span>)}
+        </Header.Subheader>
+      )
+    }
+    subheader = (
+      <div>
+        <Header.Subheader>
+          {forum.description}
+        </Header.Subheader>
+        {tags}
+      </div>
+    )
+
     return (
-      <Segment color="blue">
+      <Segment attached={attached} color="blue">
         <Grid>
           <Grid.Row>
-            <Grid.Column width={8}>
-              <Header size='huge' key={forum._id}>
-                {forum.name}
-                <Header.Subheader>
-                  {forum.description}
-                </Header.Subheader>
-              </Header>
+            <Grid.Column width={12}>
+              <Header
+                size='huge'
+                key={forum._id}
+                content={forum.name}
+                subheader={subheader}
+              />
             </Grid.Column>
-            <Grid.Column width={8} textAlign="right">
+            <Grid.Column width={4} textAlign="right">
               Managed by
               <br/>
               <AccountLink username='chainbb' />
