9515f440aefa9fdc2cbb6cc8d9a61c18e03d45e3
Aaron Cox <aaron.cox@greymass.com>
2017-06-29 17:46:21 -0400

    User avatars link to chainBB

--- services/frontend/src/components/elements/vote/avatars.js
+++ services/frontend/src/components/elements/vote/avatars.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
 import { Popup } from 'semantic-ui-react'
 
 export default class VoterAvatars extends React.Component {
@@ -7,13 +7,13 @@ export default class VoterAvatars extends React.Component {
   render() {
     const { accounts, votes } = this.props;
     const elements = accounts.slice(0,10).map((account, i) => (
-      <a href={`https://steemit.com/@${account}`} target='_blank' key={i}>
+      <Link to={`/@${account}`} key={i}>
         <Popup
           trigger={<img key={i} alt='{account}' src={`https://img.steemconnect.com/@${account}?size=35`} className="ui rounded image" />}
           position='bottom center'
           content={`@${account} (${votes[account]/100}%)`}
         />
-      </a>
+      </Link>
     ))
     return (<span>{elements}</span>)
   }
