dfbd62fb14d53deb9b20fdd746ca4224a0380595
Aaron Cox <aaron.cox@greymass.com>
2017-10-21 14:38:30 -0400

    Reduced size of pagination and put in-line

--- services/frontend/src/components/elements/forum/post/paginator.js
+++ services/frontend/src/components/elements/forum/post/paginator.js
@@ -11,32 +11,32 @@ export default class Paginator extends Component {
     if(pages <= 1) return false;
     if(pages > 1) {
       elements.push((
-        <List.Item key={1}>
+        <List.Item key={1}  style={{margin: '0 0.35em'}}>
           <Link to={`${url}#comments-page-1`}>1</Link>
         </List.Item>
       ))
       elements.push((
-        <List.Item key={2}>
+        <List.Item key={2}  style={{margin: '0 0.35em'}}>
           <Link to={`${url}#comments-page-2`}>2</Link>
         </List.Item>
       ))
     }
     if(pages > 2 && pages < 6) {
       elements.push((
-        <List.Item key={3}>
+        <List.Item key={3}  style={{margin: '0 0.35em'}}>
           <Link to={`${url}#comments-page-3`}>3</Link>
         </List.Item>
       ))
       if(pages > 3) {
         elements.push((
-          <List.Item key={4}>
+          <List.Item key={4}  style={{margin: '0 0.35em'}}>
             <Link to={`${url}#comments-page-4`}>4</Link>
           </List.Item>
         ))
       }
       if(pages > 4) {
         elements.push((
-          <List.Item key={5}>
+          <List.Item key={5}  style={{margin: '0 0.35em'}}>
             <Link to={`${url}#comments-page-5`}>5</Link>
           </List.Item>
         ))
@@ -44,28 +44,28 @@ export default class Paginator extends Component {
     }
     if(pages >= 6) {
       elements.push((
-        <List.Item key={'gap'}>
+        <List.Item key={'gap'}  style={{margin: '0 0.35em'}}>
           ...
         </List.Item>
       ))
       elements.push((
-        <List.Item key={'2nd-last'}>
+        <List.Item key={'2nd-last'}  style={{margin: '0 0.35em'}}>
           <Link to={`${url}#comments-page-${pages-1}`}>{pages-1}</Link>
         </List.Item>
       ))
       elements.push((
-        <List.Item key={'last'}>
+        <List.Item key={'last'}  style={{margin: '0 0.35em'}}>
           <Link to={`${url}#comments-page-${pages}`}>{pages}</Link>
         </List.Item>
       ))
     }
     return (
-      <div style={{marginLeft: '1.25em'}}>
-        <List horizontal size='small'>
-          <List.Item>Pages</List.Item>
-          {elements}
-        </List>
-      </div>
+      <List horizontal size='small' style={{margin: '0 0 0 0.35em'}}>
+        <List.Item style={{margin: '0'}}>
+          • Page
+        </List.Item>
+        {elements}
+      </List>
     )
   }
 }
