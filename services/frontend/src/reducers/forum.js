c4d7b4f0cf1f9c3d07fa8822ef94b2c776db4e6f
Aaron Cox <aaron.cox@greymass.com>
2017-10-26 18:08:06 -0400

    new state for forums

--- services/frontend/src/reducers/forum.js
+++ services/frontend/src/reducers/forum.js
@@ -3,6 +3,14 @@ import _ from 'lodash'
 
 export default function forum(state = {last: null}, action) {
   switch(action.type) {
+    case types.FORUM_LOAD_RESOLVED:
+      return Object.assign({}, state, {
+          target: action.payload.forum,
+          funding: {
+              'history': (action.payload.data && action.payload.data.history) ? action.payload.data.history : [],
+              'contributors': (action.payload.data && action.payload.data.contributors) ? action.payload.data.contributors : [],
+          }
+      })
     case types.FORUM_RESERVATION_PROCESSING:
     case types.FORUM_RESERVATION_RESOLVED:
     case types.FORUM_RESERVATION_RESOLVED_ERROR:
